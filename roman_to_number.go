7ea4b939205bf497144cf36a23f54a1c92e33fc4
Sem <semia.ladjal@gmail.com>
2015-02-04 10:48:44 +0100

    first commit

---
+++ roman_to_number.go
@@ -0,0 +1,15 @@
+package romanToNumber
+
+func RomanToNumber (roman string) (number int) {
+  if roman =="II"{
+    number = 2
+    }
+       if roman == "III"{
+        number = 3
+    } 
+
+    if  roman == "I"{    
+        number = 1
+    }
+return number
+}
---
+++ roman_to_number_test.go
@@ -0,0 +1,23 @@
+package romanToNumber
+
+import "testing"
+
+func TestRomanToNumber(t *testing.T) {
+  tests := []struct {
+    input string
+    expected int
+  } {
+    {input: "I", expected: 1},
+    {input: "II", expected: 2},
+    {input: "II", expected: 2},
+    
+  }
+
+  for _, testCase := range tests {
+    actual := RomanToNumber(testCase.input)
+    expected := testCase.expected
+    if actual != expected {
+      t.Errorf("%d != %d", actual, expected)
+    }
+  }
+}
