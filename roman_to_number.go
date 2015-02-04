62b9c0f525cded7d1d3ebdb470707e033a42199d
Sem <semia.ladjal@gmail.com>
2015-02-04 10:53:41 +0100

    refact

--- roman_to_number.go
+++ roman_to_number.go
@@ -1,15 +1,8 @@
 package romanToNumber
 
 func RomanToNumber (roman string) (number int) {
-  if roman =="II"{
-    number = len(roman)
-    }
-       if roman == "III"{
-        number = len(roman)
-    } 
 
-    if  roman == "I"{    
-        number = len(roman)
-    }
+       number = len(roman)
+ 
 return number
 }
