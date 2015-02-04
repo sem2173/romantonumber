e01b1c82d2f3a718a627c83cb398e62cbdbd323b
Sem <semia.ladjal@gmail.com>
2015-02-04 12:18:23 +0100

    for range

--- roman_to_number.go
+++ roman_to_number.go
@@ -4,20 +4,9 @@ func RomanToNumber (roman string) (number int) {
 
     dico := map[string]int{"I":1, "V":5}
 
-    for i := 0; i <len(roman); i++{
-
-        number = dico[roman]
-    }        
+   for _, romanletter :=range roman {
+        number += dico[string(romanletter)]
+    }     
     
-
-    if roman == "I"+"I"{
-        number = dico["I"] + dico["I"]
-    }
-
-    if roman == "I"+"I"+"I"{
-     number = dico["I"] + dico["I"] + dico["I"]
-    }
-
-     
 return number
 }
