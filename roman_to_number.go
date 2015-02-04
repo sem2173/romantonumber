f22779736ff7e40d2794214dc5840ad98add7048
Sem <semia.ladjal@gmail.com>
2015-02-04 11:13:41 +0100

    delete duplication

--- roman_to_number.go
+++ roman_to_number.go
@@ -4,20 +4,20 @@ func RomanToNumber (roman string) (number int) {
 
     dico := map[string]int{"I":1, "V":5}
 
-    if roman== "I"{
-        number = dico["I"]
+    if roman == "I"{
+        number = dico[roman]
     }
 
-    if roman== "I"+"I"{
+    if roman == "I"+"I"{
         number = 1+1
     }
 
-    if roman== "I"+"I"+"I"{
+    if roman == "I"+"I"+"I"{
      number = 1+1+1
     }
 
-    if roman== "V"{
-        number = dico["V"]
+    if roman == "V"{
+        number = dico[roman]
     }
  
 return number
